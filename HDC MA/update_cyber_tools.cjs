b3c5d8fd053185acfb59887389154991bdb5e895
horizondigitalcore <info@horizondigitalcore.com>
2026-06-27 14:10:41 +0530

    Delete HDC MA directory

--- HDC MA/update_cyber_tools.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('c:/HDC MA/HDC MA/src/data/servicesData.js', 'utf8');
-code = code.replace(
-  /techStack: \["Zscaler".*?\],/,
-  'techStack: ["Qualys", "Tenable Nessus", "OpenVAS", "Rapid7 InsightVM", "Tanium", "Splunk Enterprise Security", "IBM QRadar", "Microsoft Sentinel", "Palo Alto Cortex XSOAR", "Seceon", "CrowdStrike Falcon", "Microsoft Defender for Endpoint", "RSA Archer", "ServiceNow", "Claude Code Security"],'
-);
-fs.writeFileSync('c:/HDC MA/HDC MA/src/data/servicesData.js', code);
-console.log('Update complete');
